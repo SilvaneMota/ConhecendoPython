a43408fef8c5c7c8672c99d2d0e05490f5cab82c
unknown <silvanemota@outlook.com>
2021-05-25 16:36:00 -0300

    Capitulo 5 finalizado

--- Cap. 05/manipulaXML_start.py
+++ Cap. 05/manipulaXML_start.py
@@ -3,3 +3,15 @@
 #
 
 import xml.dom.minidom
+
+def manipulaXML():
+    doc = xml.dom.minidom.parse("C:\\Users\\Silvane\\Desktop\\Projetos Python\\ConhecendoPython\\Cap. 05\\exemploXML.xml")
+
+    print("Nome da primeira tag: ", str(doc.firstChild.tagName))
+    primeiroNome = doc.getElementsByTagName("firstname")
+    print("O primeiro nome é: ", primeiroNome[0].firstChild.nodeValue)
+
+    for skill in doc.getElementsByTagName("skill"):
+        print("Skill encontrado: " , skill.getAttribute("name"))
+
+manipulaXML()
