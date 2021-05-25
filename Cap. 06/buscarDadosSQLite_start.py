83f563f8e1997811e25625f11ae46e5d09173876
unknown <silvanemota@outlook.com>
2021-05-25 17:11:43 -0300

    Capitulo 6 finalizado

--- Cap. 06/buscarDadosSQLite_start.py
+++ Cap. 06/buscarDadosSQLite_start.py
@@ -4,3 +4,28 @@
 
 import sqlite3
 
+def manipulaDados(bancoDados, comando):
+    conexao = sqlite3.connect(bancoDados)
+    cursor = conexao.cursor()
+    cursor.execute(comando)
+    conexao.commit()
+    conexao.close()
+
+def selecionaDados(bancoDados, comando):
+    conexao = sqlite3.connect(bancoDados)
+    cursor = conexao.cursor()
+    cursor.execute(comando)
+    linhas = cursor.fetchall()
+
+    for linha in linhas:
+        print(linha)
+
+def ManipulacaoDados():
+    comandoInsert = "INSERT INTO estados (nome_estado, sigla_estado, nome_capital) VALUES ('Estado Teste', 'XX', 'Teste Inclusão')"
+    pathBD = "C:\\Users\\Silvane\\Desktop\\Projetos Python\\ConhecendoPython\\Cap. 06\\BancoDeDados.db"
+    comandoSELECT = "SELECT nome_estado, sigla_estado, nome_capital FROM estados"
+
+    manipulaDados(pathBD, comandoInsert)
+    selecionaDados(pathBD, comandoSELECT)
+
+ManipulacaoDados()
